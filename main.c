c6dae4c392a191e1b569628585f344516fa902ca
davimaltez <maltezdavid438@gmail.com>
2026-08-28 15:39:17 -0300

    Refactor complex number calculations and update logging for improved accuracy

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 #define x_min -2.0
 #define x_max 1.0
@@ -18,34 +19,40 @@ C* converter_pixel_para_complexo(int Px, int Py, int largura, int altura, C * c 
 
     //parte imaginária
     //Lembrar que a parte imaginária vem com aquele i, então eu não posso simplesmente somar com a parte real
-    float y = y_max - ((float)Py/altura) * (y_max - y_min);
+    float y = y_max - (Py/(float)altura) * (y_max - y_min);
 
     c->x = x;
     c->y = y;
+
 
     return c;
 }
 
 void calcular_normal(int Px, int Py, int largura, int altura, int max_interacoes, C * c){
 
-    float z_real = 0.0;
-    float z_imaginario = 0.0;
+    double z_real = 0.0;
+    double z_imaginario = 0.0;
 
     C * ponto_c = converter_pixel_para_complexo(Px, Py, largura, altura, c);
 
-    float c_real = c->x;
-    float c_imaginario = c->y;
+    double c_real = c->x;
+    double c_imaginario = c->y;
 
     double z_novo[2];
+    z_novo[0] = 0.0;
+    z_novo[1] = 0.0;
 
     //Fórmula para eu aplicar no loop: z = z^2 + c, onde c vai ser o retorno da função converter
     for(int i =  0; i < max_interacoes; i++){
 
         //Parte Real
-        z_novo[0] += (z_real * z_real) + c_real - (z_imaginario * z_imaginario);
+        z_novo[0] = (z_real * z_real) + c_real - (z_imaginario * z_imaginario);
 
         //Parte Imaginária
-        z_novo[1] += (2 * z_real * z_imaginario) + c_imaginario;
+        z_novo[1] = (2 * z_real * z_imaginario) + c_imaginario;
+
+        z_real = z_novo[0];
+        z_imaginario = z_novo[1];
 
     }
 
@@ -53,12 +60,12 @@ void calcular_normal(int Px, int Py, int largura, int altura, int max_interacoes
 }
 
 int main(void){ 
-    C * c;
-    int Px = 700;
-    int Py = 300;
+    C * c = (C*)malloc(sizeof(C));
+    int Px = 0;
+    int Py = 0;
     int largura = 800;
     int altura = 600;
-    int max_interacoes = 5;
+    int max_interacoes = 1;
 
     calcular_normal(Px, Py, largura, altura, max_interacoes, c);
 
